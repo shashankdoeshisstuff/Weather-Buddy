f1dfd2dd65364e3630da394f43881b0d17c6ba4b
shashank maurya <moonshader0@gmail.com>
2024-04-04 11:54:35 +0530

    created CitySelector components to search for city and get data returned from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import './App.scss';
+import CitySelector from './components/CitySelector.jsx';
 import WeatherCard from './components/WeatherCard.jsx'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Weather</div>
-         
-      </header>
+      <CitySelector/>
       {/* dt is in unix-seconds but javascript uses milliseconds, multiply with 1000 */}
-      <WeatherCard 
+      {/* <WeatherCard 
         dt={1602104400 * 1000}
         temp_min="22.67"
         temp_max="24.39"
         main="Clear"
         icon="01d"
-      />
+      /> */}
     </div>
   );
 }
--- src/components/CitySelector.jsx
+++ src/components/CitySelector.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+// importing api, api key
+import {API_KEY, API_BASE_URL} from '../apis/config';
 
 const CitySelector = () => {
+    const [city, setCity] = useState('');
+    // to display our data
+    const [results, setResult] = useState(null);
+
+    // Fetching promise we need to.
+    const onSearch = () => {
+        fetch(`${ API_BASE_URL}/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`)
+            .then((response) => response.json())
+            .then((results) => setResult(results));
+    };
+
+    // implementing to show our data when the user presses the Enter key
+    const onKeyDown = (event) => {
+        if (event.keyCode === 13) {
+            onSearch();
+        }
+    }
+
   return (
-    <div>CitySelector</div>
+    <div className='city-selector'>
+        <h1>Search your City</h1>
+        <input type="text" placeholder='Enter city'
+            /* update city value with users input */
+            onChange={(event) => setCity(event.target.value)}
+            /* value will be the currrent select city */
+            value={city}
+            // add onKeyDown
+            onKeyDown={onKeyDown}
+        />
+        {/* event handler for button click */}
+        <button 
+            onClick={onSearch}
+        >Check Weather</button>
+    </div>
   )
 }
 
-export default CitySelector
+export default CitySelector;
