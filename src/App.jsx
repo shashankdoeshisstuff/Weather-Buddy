8f78d3ef53eeadad059daaccefa0f4b21c14e45f
shashank maurya <moonshader0@gmail.com>
2024-04-06 12:23:45 +0530

    added envirnment varible

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import './App.scss';
 import CitySelector from './components/CitySelector.jsx';
 import UseFetch from './hooks/UseFetch.jsx';
-import { API_KEY, API_BASE_URL } from './apis/config.js';
+// import { API_KEY, API_BASE_URL } from './apis/config.js';
 import WeatherList from './components/WeatherList.jsx';
 
 const App = () => {
+  // import env 
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+
+  console.log(API_KEY, API_BASE_URL)
   // destructure the returned values
   const {data, error, isLoading, setUrl} = UseFetch();
 
   // error handling and loading
   const getContent = () => {
-    if(error) return <h2>Error when fetching: {error}</h2>;
+    if(error) return <h2>Error when fetching: {error.message}</h2>;
     if(!data && isLoading) return <h2>LOADING...</h2>;
     if(!data) return null;
     return <WeatherList weathers={data.list} />
